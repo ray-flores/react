687079d9dc7eeca792a4f37c0d9a10807e520487
Ray Flores <ray.m.flores@gmail.com>
2022-03-24 02:48:20 -0400

    Create cards array, shuffle function, and states for turns and cards

--- memory-game-app/src/App.js
+++ memory-game-app/src/App.js
@@ -1,11 +1,39 @@
-
+import { useState } from 'react';
 import './App.css';
 
+// card array
+const cardImages = [
+  { "src": "/images/bowser.png" },
+  { "src": "/images/donkey-kong.png" },
+  { "src": "/images/luigi.png" },
+  { "src": "/images/mario.png" },
+  { "src": "/images/peach.png" },
+  { "src": "/images/toad.png" },
+  { "src": "/images/yoshi.png" }
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // duplicate cards once and shuffle
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+  setCards(shuffleCards)
+  setTurns(0)
+  
+
+  }
+
+
+
   return (
     <div className="App">
       <h1>Magic Memory Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
